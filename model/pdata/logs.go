2fe81a574ffda72499ef217096a4f0cea18b5521
Dmitry <anoshindx@gmail.com>
2022-02-08 11:54:02 -0800

    Update Contributing guideline to include version in deprecation notice
    And update existing deprecation notices appropriately

--- model/pdata/logs.go
+++ model/pdata/logs.go
@@ -96,15 +96,15 @@ func (ld Logs) ResourceLogs() ResourceLogsSlice {
 	return newResourceLogsSlice(&ld.orig.ResourceLogs)
 }
 
-// Deprecated: use LogRecordSlice
+// Deprecated: [as of v0.44.0] use LogRecordSlice
 type LogSlice = LogRecordSlice
 
-// Deprecated: use `LogRecords`.
+// Deprecated: [as of v0.44.0] use `LogRecords`.
 func (ms InstrumentationLibraryLogs) Logs() LogRecordSlice {
 	return ms.LogRecords()
 }
 
-// Deprecated: use NewLogRecordSlice
+// Deprecated: [as of v0.44.0] use NewLogRecordSlice
 func NewLogSlice() LogRecordSlice {
 	return NewLogRecordSlice()
 }
